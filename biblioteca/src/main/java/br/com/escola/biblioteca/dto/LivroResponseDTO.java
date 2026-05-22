3580d17c8b69677516b17ea5f2d46ffc5567b215
Danielle-Carvalho <danielle.c.ferreira7@aluno.senai.br>
2026-05-22 20:22:53 -0300

    feat: adicionando DTO e resolvendo conflitos

--- biblioteca/src/main/java/br/com/escola/biblioteca/dto/LivroResponseDTO.java
+++ biblioteca/src/main/java/br/com/escola/biblioteca/dto/LivroResponseDTO.java
@@ -1,5 +1,25 @@
 package br.com.escola.biblioteca.dto;
 
-public class LivroResponseDTO {
-    
+import br.com.escola.biblioteca.entity.Livro;
+
+public record LivroResponseDTO(
+    Long id,
+    String titulo,
+    String isbn,
+    Integer anoPublicacao,
+    String genero,
+    Long autorId,
+    String autorNome
+) {
+    public static LivroResponseDTO fromEntity(Livro livro) {
+        return new LivroResponseDTO(
+            livro.getId(),
+            livro.getTitulo(),
+            livro.getIsbn(),
+            livro.getAnoPublicacao(),
+            livro.getGenero(),
+            livro.getAutor().getId(),
+            livro.getAutor().getNome()
+        );
+    }
 }
