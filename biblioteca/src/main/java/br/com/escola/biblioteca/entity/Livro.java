54e910ebc74203e167e3859eef71a6f8dafc9ec1
Leonardo de Mattos <leodemattos@gmail.com>
2026-05-22 18:54:54 -0300

    Adicionando livro entity

--- biblioteca/src/main/java/br/com/escola/biblioteca/entity/Livro.java
+++ biblioteca/src/main/java/br/com/escola/biblioteca/entity/Livro.java
@@ -1,5 +1,51 @@
-package main.java.br.com.escola.biblioteca.entity;
+package br.com.escola.biblioteca.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "livro")
 public class Livro {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    private String isbn;
+    private Integer anoPublicacao;
+    private String genero;
+
+    @ManyToOne
+    @JoinColumn(name = "autor_id", nullable = false) 
+    private Autor autor;
+
+    public Livro() {}
+
+    public Livro(String titulo, String isbn, Integer anoPublicacao, String genero, Autor autor) {
+        this.titulo = titulo;
+        this.isbn = isbn;
+        this.anoPublicacao = anoPublicacao;
+        this.genero = genero;
+        this.autor = autor;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getTitulo() { return titulo; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+
+    public String getIsbn() { return isbn; }
+    public void setIsbn(String isbn) { this.isbn = isbn; }
+
+    public Integer getAnoPublicacao() { return anoPublicacao; }
+    public void setAnoPublicacao(Integer anoPublicacao) { this.anoPublicacao = anoPublicacao; }
+
+    public String getGenero() { return genero; }
+    public void setGenero(String genero) { this.genero = genero; }
+
+    public Autor getAutor() { return autor; }
+    public void setAutor(Autor autor) { this.autor = autor; }
 }
