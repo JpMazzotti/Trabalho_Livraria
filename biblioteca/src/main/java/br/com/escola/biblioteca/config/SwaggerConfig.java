3f857fb798fd3f85fd2b08effdd7793e125f0aac
Leonardo de Mattos <leodemattos@gmail.com>
2026-05-22 22:48:20 -0300

    Criando swagger

--- biblioteca/src/main/java/br/com/escola/biblioteca/config/SwaggerConfig.java
+++ biblioteca/src/main/java/br/com/escola/biblioteca/config/SwaggerConfig.java
@@ -1,5 +1,19 @@
 package br.com.escola.biblioteca.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
-    
+
+    @Bean
+    public OpenAPI bibliotecaOpenAPI() {
+       return new OpenAPI()
+        .info(new Info()
+        .title("API de Catálogo de Livros e Autores")
+        .version("1.0")
+        .description("Trabalho API restful - Grupo 5 - Sistema de Biblioteca Simples"));
+    }
 }
