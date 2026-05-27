d79a6d31361009918e73732b2b582eeac76d005f
JpMazzotti <mazzotti0202@gmail.com>
2026-05-27 17:02:03 -0300

    Arrumando o lugar das validações

--- biblioteca/src/main/java/br/com/escola/biblioteca/dto/GeneroRequestDTO.java
+++ biblioteca/src/main/java/br/com/escola/biblioteca/dto/GeneroRequestDTO.java
@@ -1,8 +1,13 @@
 package br.com.escola.biblioteca.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record GeneroRequestDTO(
-        
+        @NotBlank(message = "O nome do gênero é obrigatório")
         String nome,
+        
+        @NotBlank(message = "A sigla é obrigatória")
+        @Size(min = 3, max = 3, message = "A sigla deve ter exatamente 3 caracteres")
         String sigla
 ) {}
