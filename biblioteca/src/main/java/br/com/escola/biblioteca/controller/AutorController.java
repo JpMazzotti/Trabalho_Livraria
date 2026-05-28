9979d9ca9570a21d8f9281d752af5166e21aad7e
JpMazzotti <mazzotti0202@gmail.com>
2026-05-28 12:02:31 -0300

    Ajustes no Swagger

--- biblioteca/src/main/java/br/com/escola/biblioteca/controller/AutorController.java
+++ biblioteca/src/main/java/br/com/escola/biblioteca/controller/AutorController.java
@@ -3,6 +3,7 @@ package br.com.escola.biblioteca.controller;
 import br.com.escola.biblioteca.dto.AutorRequestDTO;
 import br.com.escola.biblioteca.dto.AutorResponseDTO;
 import br.com.escola.biblioteca.service.AutorService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/autores")
+@SecurityRequirement(name = "bearerAuth")
 public class AutorController {
 
     private final AutorService autorService;
